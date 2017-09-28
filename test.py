fa0254a38cc86b8b7e4448fb122b475fc5a0387c
josegob <jose.gob@gmail.com>
2017-09-28 16:08:27 +0200

    Modificado test.py para pruebas

--- test.py
+++ test.py
@@ -1,21 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-import funciones_bot
 import unittest
 
 
 class TestsBotTelegram(unittest.TestCase):
 
-    def test_url(self):
-        self.assertEqual(funciones_bot.buscadorJuegos('juego prueba test'), -1)
-        self.assertEqual(funciones_bot.buscadorSeries('serie prueba test'), -1)
-        self.assertEqual(funciones_bot.buscadorJuegos('pelicula prueba test'), -1)
-
-    def test_listas(self):
-        self.assertEqual(len(funciones_bot.buscadorJuegos('life is strange')), 6)
-        self.assertEqual(len(funciones_bot.buscadorSeries('young sheldon')), 6)
-        self.assertEqual(len(funciones_bot.buscadorPeliculas('american made')), 6)
+    def test_prueba(self):
+        self.assertEqual(2+3,5)
 
 
 if __name__ == '__main__':
