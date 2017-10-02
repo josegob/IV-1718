178b082d0c09a450d31dc65747f6f2618a5bb786
josegob <jose.gob@gmail.com>
2017-10-02 13:44:00 +0200

    Añadidos dos tests adicionales

--- bot_metacritic/test.py
+++ bot_metacritic/test.py
@@ -10,7 +10,10 @@ class TestsBotTelegram(unittest.TestCase):
     def test_url(self):
         self.assertEqual(funciones_bot.buscadorJuegos('juego prueba test'), -1)
         self.assertEqual(funciones_bot.buscadorSeries('serie prueba test'), -1)
-        self.assertEqual(funciones_bot.buscadorJuegos('pelicula prueba test'), -1)
+        self.assertEqual(funciones_bot.buscadorPeliculas('pelicula prueba test'), -1)
+        self.assertEqual(funciones_bot.buscadorJuegos(''), -1)
+        self.assertEqual(funciones_bot.buscadorSeries(' '), -1)
+        self.assertEqual(funciones_bot.buscadorPeliculas('         '), -1)
 
     def test_listas(self):
         self.assertEqual(len(funciones_bot.buscadorJuegos('life is strange')), 6)
@@ -20,3 +23,4 @@ class TestsBotTelegram(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
