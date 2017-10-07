04b079614f661c3fa7c272f8c4afe81e83b27706
josegob <jose.gob@gmail.com>
2017-10-07 18:34:44 +0200

    Añadido nuevos comandos

--- bot_metacritic/bot_metacritic.py
+++ bot_metacritic/bot_metacritic.py
@@ -31,7 +31,7 @@ def command_buscarjuego(m):
     if (juego_busqueda == ''):
         bot.send_message(cid, 'Debes especificar el nombre del juego junto al comando')
     else:
-        bot.send_message( cid, 'Un momento, voy a buscar el juego')
+        bot.send_message(cid, 'Un momento, voy a buscar el juego')
         resultado_busqueda = buscadorJuegos(juego_busqueda)
         if (resultado_busqueda == -1):
             bot.send_message(cid, 'Illo lo siento. El juego introducido no se encuentra en la base de datos de Metacritic')
@@ -45,6 +45,57 @@ def command_buscarjuego(m):
             bot.send_message(cid, 'Desarrolladora: ' + resultado_busqueda[4])
             bot.send_message(cid, 'Enlace: ' + resultado_busqueda[5])
             resultado_busqueda[:] = []
+
+
+@bot.message_handler(commands=['buscarserie'])
+def command_buscarserie(m):
+    resultado_busqueda_serie = []
+    cid = m.chat.id
+    serie_busqueda = m.text
+    serie_busqueda = re.sub(r'\s+', ' ', serie_busqueda)
+    serie_busqueda = serie_busqueda[13:]
+    if (serie_busqueda == ''):
+        bot.send_message(cid, 'Debes especificar el nombre de la serie junto al comando')
+    else:
+        bot.send_message(cid, 'Un momento, voy a buscar la serie')
+        resultado_busqueda_serie = buscadorSeries(serie_busqueda)
+        if (resultado_busqueda_serie == -1):
+            bot.send_message(cid, 'Illo lo siento. La serie introducida no se encuentra en la base de datos de Metacritic')
+
+        else:
+            bot.send_message(cid, 'Nombre de la serie: ' + resultado_busqueda_serie[0])
+            bot.send_message(cid, 'Nota Metacritic: ' + resultado_busqueda_serie[1])
+            bot.send_message(cid, 'Nota media usuarios: ' + resultado_busqueda_serie[2])
+            bot.send_message(cid, 'Fecha de lanzamiento: ' + resultado_busqueda_serie[3])
+            bot.send_message(cid, 'Productora: ' + resultado_busqueda_serie[4])
+            bot.send_message(cid, 'Enlace: ' + resultado_busqueda_serie[5])
+            resultado_busqueda_serie[:] = []
+
+
+@bot.message_handler(commands=['buscarpelicula'])
+def command_buscarpelicula(m):
+    resultado_busqueda_pelicula = []
+    cid = m.chat.id
+    pelicula_busqueda = m.text
+    pelicula_busqueda = re.sub(r'\s+', ' ', pelicula_busqueda)
+    pelicula_busqueda = pelicula_busqueda[16:]
+    if (pelicula_busqueda == ''):
+        bot.send_message(cid, 'Debes especificar el nombre de la pelicula junto al comando')
+    else:
+        bot.send_message(cid, 'Un momento, voy a buscar la pelicula')
+        resultado_busqueda_pelicula = buscadorPeliculas(pelicula_busqueda)
+        if (resultado_busqueda_pelicula == -1):
+            bot.send_message(cid, 'Illo lo siento. La serie introducida no se encuentra en la base de datos de Metacritic')
+
+        else:
+            bot.send_message(cid, 'Nombre de la pelicula: ' + resultado_busqueda_pelicula[0])
+            bot.send_message(cid, 'Nota Metacritic: ' + resultado_busqueda_pelicula[1])
+            bot.send_message(cid, 'Nota media usuarios: ' + resultado_busqueda_pelicula[2])
+            bot.send_message(cid, 'Fecha de lanzamiento: ' + resultado_busqueda_pelicula[3])
+            bot.send_message(cid, 'Productora: ' + resultado_busqueda_pelicula[4])
+            bot.send_message(cid, 'Enlace: ' + resultado_busqueda_pelicula[5])
+            resultado_busqueda_pelicula[:] = []
+
 
 @bot.message_handler(commands=['help', 'ayuda'])
 def command_help(m):
