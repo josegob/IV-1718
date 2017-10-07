5aa1fb818864b8468b76a6e24ffa90de62d6c585
josegob <jose.gob@gmail.com>
2017-10-07 17:04:43 +0200

    Añadido gestor top20 Bot

---
+++ bot_metacritic/gestor_info_bd.py
@@ -0,0 +1,42 @@
+import psycopg2
+import top20
+import os
+from urllib import parse
+
+peliculas = top20.top20Peliculas()
+series = top20.top20Series()
+
+parse.uses_netloc.append("postgres")
+url = parse.urlparse(os.environ["DATABASE_URL"])
+
+# Connect to an existing database
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
+
+# Open a cursor to perform database operations
+cur = conn.cursor()
+
+cur.execute("DELETE from top20peliculas")
+cur.execute("DELETE from top20series")
+
+for j in range(0,20):
+    cur.execute("INSERT INTO top20peliculas (nombre_pelicula,puntuacion_pelicula,enlace_pelicula,fecha_pelicula) VALUES (%s,%s,%s,%s)", (peliculas[0][j],peliculas[1][j],peliculas[2][j],peliculas[3][j]))
+
+for p in range(0,20):
+    cur.execute("INSERT INTO top20series (nombre_serie,puntuacion_serie,enlace_serie,fecha_serie) VALUES (%s,%s,%s,%s)", (series[0][p],series[1][p],series[2][p],series[3][p]))
+# Pass data to fill a query placeholders and let Psycopg perform
+# the correct conversion (no more SQL injections!)
+
+# Query the database and obtain data as Python objects
+
+# Make the changes to the database persistent
+conn.commit()
+
+# Close communication with the database
+cur.close()
+conn.close()
