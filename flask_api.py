41c359499eee2db7c3ae5609a47b13feea41cf0d
josegob <jose.gob@gmail.com>
2017-10-24 13:10:40 +0200

    Cambios de diseño en la API

--- flask_api.py
+++ flask_api.py
@@ -3,32 +3,43 @@ from flask import Flask, request
 from bot_metacritic.funciones_bot import *
 import json
 from bot_metacritic.funciones_top20 import *
+from flask_restful import Resource, Api
+
 
 app = Flask(__name__)
+api = Api(app)
 
 @app.route("/")
 def welcome():
     return ("API BOT")
 
-@app.route("/buscarjuego/<juego>")
-def buscarjuego(juego):
-    return json.dumps(buscadorJuegos(juego))
+class buscarjuego(Resource):
+    def get(self, juego):
+        return buscadorJuegos(juego)
 
-@app.route("/buscarpelicula/<pelicula>")
-def buscarpelicula(pelicula):
-    return json.dumps(buscadorPeliculas(pelicula))
+api.add_resource(buscarjuego, '/juego/<juego>')
 
-@app.route("/buscarserie/<serie>")
-def buscarserie(serie):
-    return json.dumps(buscadorSeries(serie))
+class buscarPelicula(Resource):
+    def get(self, pelicula):
+        return buscadorPeliculas(pelicula)
 
-@app.route("/top20peliculas")
-def top20peliculas():
-    return json.dumps(top20Peliculas())
+api.add_resource(buscarPelicula, '/pelicula/<pelicula>')
 
-@app.route("/top20series")
-def top20series():
-    return json.dumps(top20Series())
+class buscarSerie(Resource):
+    def get(self, serie):
+        return buscadorSeries(serie)
+
+api.add_resource(buscarSerie, '/serie/<serie>')
+
+@app.route('/top20/peliculas')
+def devuelveTop20Peliculas():
+    lista_p = top20Peliculas()
+    return '{} {} {} {}'.format((lista_p[0]), (lista_p[1]), (lista_p[2]), (lista_p[3]))
+
+@app.route('/top20/series')
+def devuelveTop20Series():
+    lista_s = top20Series()
+    return '{} {} {} {}'.format((lista_s[0]), (lista_s[1]), (lista_s[2]), (lista_s[3]))
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=True)
