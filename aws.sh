24d3120edf463231aac463a86fe2d2d0ea8db313
josegob <jose.gob@gmail.com>
2017-12-08 21:21:06 +0100

    Ficheros movidos a carpeta

--- aws.sh
+++
@@ -1,7 +0,0 @@
-vagrant up --provider=aws
-
-fab -i FINAL_KEY.pem -H ubuntu@DNS instalar_proyecto
-fab -i FINAL_KEY.pem -H ubuntu@DNS api_up
-
-#Cambiar FINAL_KEY.pem por el archivo correspondiente de AWS
-#Cambiar DNS por el correspondiente del a instancia creada con Vagrant
